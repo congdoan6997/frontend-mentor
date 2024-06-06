af044107b62f1602d705b0fa123d0f2a341d79da
PhDBui <congdoan6997@gmail.com>
2024-06-06 23:03:05 +0300

    feat: 🎸 nft preview card component main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./contact-form-main/*.{html,js}"],
+  content: ["./nft-preview-card-component-main/*.{html,js}"],
   theme: {
     extend: {},
     colors: {
       primary: {
-        green: {
-          lighter: "hsl(148, 38%, 91%)",
-          medium: "hsl(169, 82%, 27%)",
-        },
-        red: "hsl(0, 66%, 54%)",
+        "soft-blue": "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
       },
       neutral: {
+        "very-dark-blue-bg": "hsl(217, 54%, 11%)",
+        "very-dark-blue-card": "hsl(216, 50%, 16%)",
+        "very-dark-blue-line": "hsl(215, 32%, 27%)",
         white: "hsl(0, 0%, 100%)",
-        grey: {
-          medium: "hsl(186, 15%, 59%)",
-          darker: "hsl(187, 24%, 22%)",
-        },
       },
     },
     screens: {
