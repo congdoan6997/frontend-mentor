39f04abbdaf31c85ac28b29cb91c0a73134a4625
PhDBui <congdoan6997@gmail.com>
2024-06-06 23:40:54 +0300

    feat: 🎸 3 column preview card component main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./nft-preview-card-component-main/*.{html,js}"],
+  content: ["./3-column-preview-card-component-main/*.{html,js}"],
   theme: {
     extend: {},
     colors: {
       primary: {
-        "soft-blue": "hsl(215, 51%, 70%)",
-        cyan: "hsl(178, 100%, 50%)",
+        "bright-orange": "hsl(31, 77%, 52%)",
+        "dark-cyan": "hsl(184, 100%, 22%)",
+        "very-dark-cyan": "hsl(179, 100%, 13%)",
       },
       neutral: {
-        "very-dark-blue-bg": "hsl(217, 54%, 11%)",
-        "very-dark-blue-card": "hsl(216, 50%, 16%)",
-        "very-dark-blue-line": "hsl(215, 32%, 27%)",
-        white: "hsl(0, 0%, 100%)",
+        "transparent-white": "hsla(0, 0%, 100%, 0.75)",
+        "very-light-gray": "hsl(0, 0%, 95%)",
       },
+    },
+    fontFamily: {
+      "lexend-deca": ["Lexend Deca", "sans-serif"],
+      "big-shoulders-display": ["Big Shoulders Display", "cursive"],
     },
     screens: {
       mobile: "375px",
