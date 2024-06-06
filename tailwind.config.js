448965290320b4c30c1519bf4a892a013787a615
PhDBui <congdoan6997@gmail.com>
2024-06-06 22:20:30 +0300

    feat: 🎸 contact form main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./testimonials-grid-section-main/*.{html,js}"],
+  content: ["./contact-form-main/*.{html,js}"],
   theme: {
     extend: {},
     colors: {
       primary: {
-        violet: "hsl(263, 55%, 52%)",
-        "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
-        "very-dark-blackish-blue": "hsl(219, 29%, 14%)",
-        white: "hsl(0, 0%, 100%)",
+        green: {
+          lighter: "hsl(148, 38%, 91%)",
+          medium: "hsl(169, 82%, 27%)",
+        },
+        red: "hsl(0, 66%, 54%)",
       },
       neutral: {
-        "light-gray": "hsl(0, 0%, 81%)",
-        "light-grayish-blue": "hsl(210, 46%, 95%)",
-      },
-      custom: {
-        "verified-graduate": "rgba(63, 63, 88, 0.5)", // 50% opacity of hsl(217, 19%, 35%)
-        "review-paragraph": "rgba(63, 63, 88, 0.7)", // same as verified graduate
+        white: "hsl(0, 0%, 100%)",
+        grey: {
+          medium: "hsl(186, 15%, 59%)",
+          darker: "hsl(187, 24%, 22%)",
+        },
       },
     },
     screens: {
