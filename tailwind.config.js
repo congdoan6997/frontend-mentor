47e25496ace55dade00f9a67c8aebd86b9a3fd11
PhDBui <congdoan6997@gmail.com>
2024-06-07 12:48:31 +0300

    feat: 🎸 huddle langding page with single introductory section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./order-summary-component-main/*.{html,js}"],
+  content: [
+    "./huddle-landing-page-with-single-introductory-section-master/*.{html,js}",
+  ],
   theme: {
     extend: {},
     colors: {
       primary: {
-        "pale-blue": "hsl(225, 100%, 94%)",
-        "bright-blue": "hsl(245, 75%, 52%)",
-      },
-      neutral: {
-        "very-pale-blue": "hsl(225, 100%, 98%)",
-        "desaturated-blue": "hsl(224, 23%, 55%)",
-        "dark-blue": "hsl(223, 47%, 23%)",
+        violet: "hsl(257, 40%, 49%)",
+        "soft-magenta": "hsl(300, 69%, 71%)",
       },
     },
     fontFamily: {
-      "red-hat-display": ["Red Hat Display", "sans-serif"],
+      poppins: '"Poppins", sans-serif',
+      "open-sans": '"Open Sans", sans-serif',
     },
   },
   plugins: [],
